6600f4303eee1e0379d6cf24d09960cfaf5438b5
karthikprasad-dev <karthikprasad100k@gmail.com>
2025-12-05 10:37:04 +0530

    Added Binary Search

---
+++ Searching/binarySearch.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(vector<int> arr, int target) {
+
+    int start = 0;
+    int end = arr.size() - 1;
+
+    while(start<=end) {
+        int mid = start + (end - start) / 2;
+        if(target > arr[mid]) start = mid + 1;
+        else if(target < arr[mid]) end  = mid - 1;
+        else return mid;
+    }
+
+    return -1;
+}
+
+int main() {
+
+    vector<int> arr = {1, 4, 9, 10, 23, 55, 99};
+    cout << binarySearch(arr, 99);
+
+    return 0;
+}
