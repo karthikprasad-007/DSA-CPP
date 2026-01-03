7f9988e0636ed2475b5856c04aed14d3d52215d5
karthikprasad-dev <karthikprasad100k@gmail.com>
2026-01-03 23:51:31 +0530

    Optimised

--- Sorting/bubbleSort.cpp
+++ Sorting/bubbleSort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void bubbleSort(vector<int> &arr) {
     for(int i = 0; i < arr.size(); ++i) {
-        for(int j = 1; j < arr.size(); ++j) {
+        for(int j = 1; j < arr.size() - i; ++j) {
             if(arr[j] < arr[j-1]) {
                 int temp = arr[j];
                 arr[j] = arr[j-1];
