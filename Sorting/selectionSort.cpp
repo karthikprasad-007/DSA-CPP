ae7a4b87d36817be1ab5da18a5a19f1c90b8be17
karthikprasad-dev <karthikprasad100k@gmail.com>
2026-01-05 23:31:49 +0530

    Added selection sort

---
+++ Sorting/selectionSort.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selectionSort(vector<int> &arr) {
+    int n = arr.size();
+    for(int i = 0; i < n; ++i) {
+        int max = 0;
+        for(int j = 0; j < n - i; ++j) {
+            if(arr[j] > arr[max]) max = j;
+        }
+        int temp = arr[n-i-1];
+        arr[n-i-1] = arr[max];
+        arr[max] = temp;
+    }
+}
+
+void display(vector<int> arr) {
+    for(int i = 0; i < arr.size(); ++i) cout << arr[i] << " ";
+}
+
+int main() {
+
+    vector<int> arr = {5, 3, 1, 2, 5, 8, 4, 10, 9, 8, -1};
+    selectionSort(arr);
+    display(arr);
+    
+    return 0;
+}
