f0031f5870a9e4dabd0d340586874a0ff798afed
karthikprasad-dev <karthikprasad100k@gmail.com>
2026-01-11 23:57:32 +0530

    Added Cyclic Sort

---
+++ Sorting/cyclicSort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void cyclicSort(vector<int>& arr) {
+    int n = arr.size();
+    
+    for (int i = 0; i < n; i++) {
+        while (arr[i] != i + 1) {
+            int correctIndex = arr[i] - 1;
+            swap(arr[i], arr[correctIndex]);
+        }
+    }
+}
+
+int main() {
+    vector<int> arr = {3, 1, 2, 5, 4};
+    
+    cout << "Original array: ";
+    for (int num : arr) cout << num << " ";
+    cout << "\n";
+    
+    cyclicSort(arr);
+    
+    cout << "Sorted array: ";
+    for (int num : arr) cout << num << " ";
+    cout << "\n";
+    
+    return 0;
+}
